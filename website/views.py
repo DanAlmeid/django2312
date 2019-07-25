f993011f5dc028ced99ead10f1863388038f7688
danilo <daniloa792@gmail.com>
2019-07-25 17:03:06 -0300

    fazer formulario

---
+++ website/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from website.models import Pessoa
+# Create your views here.
+
+def index(request):
+    contexto = {}
+    if request.method == 'POST':
+        pessoa = Pessoa()
+        pessoa.nome = request.POST.get('nome')
+        pessoa.sobrenome = request.POST.get('sobrenome')
+        pessoa.email = request.POST.get('email')
+        pessoa.genero = request.POST.get('genero')
+        pessoa.save()
+        contexto = {'msg':'usuario cadastrado'}
+    return render(request,'index.html', contexto)
+
+def sobre(request):
+    pessoa = Pessoa.objects.all()
+    contexto = {
+        'pessoas':pessoa
+    }
+    return render(request, 'sobre.html', contexto)
